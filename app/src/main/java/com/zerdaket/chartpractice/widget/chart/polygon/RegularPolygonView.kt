89960cb2c7cf78b88717867c20ee8c691f921855
zerdaket <zerdaket@gmail.com>
2020-08-03 23:50:32 +0800

    新增正多边形外接圆半径和中心点

--- app/src/main/java/com/zerdaket/chartpractice/widget/chart/polygon/RegularPolygonView.kt
+++ app/src/main/java/com/zerdaket/chartpractice/widget/chart/polygon/RegularPolygonView.kt
@@ -2,8 +2,11 @@ package com.zerdaket.chartpractice.widget.chart.polygon
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.Point
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.min
 
 /**
  * @author zerdaket
@@ -11,16 +14,36 @@ import android.view.View
  */
 class RegularPolygonView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0): View(context, attrs, defStyleAttr) {
 
+    /**
+     * 外接圆半径
+     */
+    private var radius = 0
+
+    /**
+     * 中心点
+     */
+    private val centerPoint = Point()
+
+    /**
+     * 绘制路径
+     */
+    private val path = Path()
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        radius = min(w, h).div(2)
+        centerPoint.set(w.div(2), h.div(2))
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        drawPolygon(canvas)
+        drawLines(canvas)
+        drawRegion(canvas)
     }
 
     private fun drawPolygon(canvas: Canvas) {
-
+        path.reset()
     }
 
     private fun drawLines(canvas: Canvas) {
