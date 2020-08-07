153a03f26a6b163ce2dbd2ad389d3eff0b058e47
zerdaket <zerdaket@gmail.com>
2020-08-07 23:45:00 +0800

    新增直方图

---
+++ app/src/main/java/com/zerdaket/chartpractice/widget/chart/HistogramView.kt
@@ -0,0 +1,22 @@
+package com.zerdaket.chartpractice.widget.chart
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @author zerdaket
+ * @date 2020/8/7 11:44 PM
+ */
+class HistogramView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0): View(context, attrs, defStyleAttr) {
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+}
