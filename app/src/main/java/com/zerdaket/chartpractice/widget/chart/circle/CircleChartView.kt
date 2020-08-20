937f89c91fdc0f56e3bd53bb9c4d79c8c7b00318
zerdaket <zerdaket@gmail.com>
2020-08-20 23:46:16 +0800

    新增圆形图表

---
+++ app/src/main/java/com/zerdaket/chartpractice/widget/chart/circle/CircleChartView.kt
@@ -0,0 +1,25 @@
+package com.zerdaket.chartpractice.widget.chart.circle
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @author zerdaket
+ * @date 2020/8/20 11:34 PM
+ */
+class CircleChartView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0): View(context, attrs, defStyleAttr) {
+
+    private val mainPaint = Paint()
+    private val textPaint = Paint()
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+}
